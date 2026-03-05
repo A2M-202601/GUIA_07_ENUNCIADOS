dfe0cc34eedcca9f8070168fd9af413b7c3e4648
abrildiazmiguez <adiazm@fi.uba.ar>
2026-03-05 18:50:55 -0300

    Actualizo enunciado

--- 02 - ej iteradores 1 - numeros impares/abb.h
+++ 02 - ej iteradores 1 - numeros impares/abb.h
@@ -1,112 +1,107 @@
-#ifndef __ARBOL_BINARIO_DE_BUSQUEDA_H__
-#define __ARBOL_BINARIO_DE_BUSQUEDA_H__
-
-#define ABB_RECORRER_INORDEN   0
-#define ABB_RECORRER_PREORDEN  1
-#define ABB_RECORRER_POSTORDEN 2
+#ifndef __ABB__H__
+#define __ABB__H__
 
 #include <stdbool.h>
 #include <stdlib.h>
 
-/*
- * Comparador de elementos. Recibe dos elementos del arbol y devuelve
- * 0 en caso de ser iguales, >0 si el primer elemento es mayor al
- * segundo o <0 si el primer elemento es menor al segundo.
- */
-typedef int (*abb_comparador)(void*, void*);
+typedef enum { INORDEN, PREORDEN, POSTORDEN } abb_recorrido;
 
-/*
- * Destructor de elementos. Cada vez que un elemento deja el arbol
- * (arbol_borrar o arbol_destruir) se invoca al destructor pasandole
- * el elemento.
+/**
+ * Comparador de elementos. Recibe dos elementos y devuelve 0 en caso de ser
+ * iguales, >0 si el primer elemento es mayor al segundo o <0 si el primer
+ * elemento es menor al segundo.
  */
-typedef void (*abb_liberar_elemento)(void*);
-
-typedef struct nodo_abb nodo_abb_t;
+typedef int (*abb_comparador)(void *, void *);
 
 typedef struct abb abb_t;
 
-/*
- * Crea el arbol y reserva la memoria necesaria de la estructura.
- * Comparador se utiliza para comparar dos elementos.
- * Destructor es invocado sobre cada elemento que sale del arbol,
- * puede ser NULL indicando que no se debe utilizar un destructor.
+/**
+ * Crea un arbol binario de búsqueda. Comparador es una función que se utiliza
+ * para comparar dos elementos. La funcion de comparación no puede ser nula.
  *
  * Devuelve un puntero al arbol creado o NULL en caso de error.
  */
-abb_t* arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor);
+abb_t *abb_crear(abb_comparador comparador);
 
-/*
+/**
  * Inserta un elemento en el arbol.
- * Devuelve 0 si pudo insertar o -1 si no pudo.
  * El arbol admite elementos con valores repetidos.
+ *
+ * Devuelve el arbol en caso de exito o NULL en caso de error.
  */
-int arbol_insertar(abb_t* arbol, void* elemento);
+abb_t *abb_insertar(abb_t *arbol, void *elemento);
 
-/*
- * Busca en el arbol un elemento igual al provisto (utilizando la
- * funcion de comparación) y si lo encuentra lo quita del arbol.
- * Adicionalmente, si encuentra el elemento, invoca el destructor con
- * dicho elemento.
- * Devuelve 0 si pudo eliminar el elemento o -1 en caso contrario.
+/**
+ * Busca en el arbol un elemento igual al provisto (utilizando la funcion de
+ * comparación) y si lo encuentra lo quita del arbol y lo devuelve.
+ *
+ * (Recordá que en este caso, las pruebas de chanutron suponen que al eliminar
+ * con dos hijos, se reemplaza el nodo con el predecesor inorden.)
+ *
+ * Devuelve el elemento extraido del árbol o NULL en caso de error.
  */
-int arbol_borrar(abb_t* arbol, void* elemento);
+void *abb_quitar(abb_t *arbol, void *elemento);
 
-/*
+/**
  * Busca en el arbol un elemento igual al provisto (utilizando la
  * funcion de comparación).
  *
  * Devuelve el elemento encontrado o NULL si no lo encuentra.
  */
-void* arbol_buscar(abb_t* arbol, void* elemento);
+void *abb_buscar(abb_t *arbol, void *elemento);
 
-/*
- * Devuelve el elemento almacenado como raiz o NULL si el árbol está
- * vacío o no existe.
- */
-void* arbol_raiz(abb_t* arbol);
-
-/*
+/**
  * Determina si el árbol está vacío.
- * Devuelve true si está vacío o el arbol es NULL, false si el árbol tiene elementos.
+ *
+ * Devuelve true si está vacío o el arbol es NULL, false si el árbol tiene al
+ * menos un elemento.
  */
-bool arbol_vacio(abb_t* arbol);
+bool abb_vacio(abb_t *arbol);
 
-/*
- * Llena el array del tamaño dado con los elementos de arbol
- * en secuencia inorden.
- * Devuelve la cantidad de elementos del array que pudo llenar (si el
- * espacio en el array no alcanza para almacenar todos los elementos,
- * llena hasta donde puede y devuelve la cantidad de elementos que
- * pudo poner).
+/**
+ * Devuelve la cantidad de elementos almacenados en el arbol o 0 si el arbol es
+ * NULL.
  */
-size_t arbol_recorrido_inorden(abb_t* arbol, void** array, size_t tamanio_array);
+size_t abb_tamanio(abb_t *arbol);
 
-/*
- * Llena el array del tamaño dado con los elementos de arbol
- * en secuencia preorden.
- * Devuelve la cantidad de elementos del array que pudo llenar (si el
- * espacio en el array no alcanza para almacenar todos los elementos,
- * llena hasta donde puede y devuelve la cantidad de elementos que
- * pudo poner).
+/**
+ * Recorre el arbol e invoca la funcion con cada elemento almacenado en el mismo
+ * como primer parámetro. El puntero aux se pasa como segundo parámetro a la
+ * función. Si la función devuelve false, se finaliza el recorrido aun si quedan
+ * elementos por recorrer. Si devuelve true se sigue recorriendo mientras queden
+ * elementos.
+ *
+ * Recorrido especifica el tipo de recorrido a realizar.
+ *
+ * Devuelve la cantidad de veces que fue invocada la función.
+*/
+size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
+			     bool (*funcion)(void *, void *), void *aux);
+
+/**
+ * Recorre el arbol según el recorrido especificado y va almacenando los
+ * elementos en el array hasta completar el recorrido o quedarse sin espacio en
+ * el array.
+ *
+ * El array tiene un tamaño maximo especificado por tamanio_array.
+ *
+ * Devuelve la cantidad de elementos que fueron almacenados exitosamente en el
+ * array.
  */
-size_t arbol_recorrido_preorden(abb_t* arbol, void** array, size_t tamanio_array);
+size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array,
+		    size_t tamanio_array);
 
-/*
- * Llena el array del tamaño dado con los elementos de arbol
- * en secuencia postorden.
- * Devuelve la cantidad de elementos del array que pudo llenar (si el
- * espacio en el array no alcanza para almacenar todos los elementos,
- * llena hasta donde puede y devuelve la cantidad de elementos que
- * pudo poner).
- */
-size_t arbol_recorrido_postorden(abb_t* arbol, void** array, size_t tamanio_array);
-
-/*
+/**
  * Destruye el arbol liberando la memoria reservada por el mismo.
- * Adicionalmente invoca el destructor con cada elemento presente en
- * el arbol.
  */
-void arbol_destruir(abb_t* arbol);
+void abb_destruir(abb_t *arbol);
 
-#endif /* __ARBOL_BINARIO_DE_BUSQUEDA_H__ */
+/**
+ * Destruye el arbol liberando la memoria reservada por el mismo.
+ *
+ * Adicionalmente invoca el destructor en cada uno de los elementos almacenados
+ * en el arbol (si la funcion destructor no es NULL).
+ */
+void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *));
+
+#endif /* __ABB__H__ */
